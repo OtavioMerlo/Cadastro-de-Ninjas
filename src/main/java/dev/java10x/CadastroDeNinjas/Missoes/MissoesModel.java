866655f6b70a96bfcd18a7b7901b7c290c85373c
OtavioMerlo <otaviomerloc@gmail.com>
2025-07-07 16:49:10 -0300

    Add a tabela Missoes Fazendo uma conexao com foreing Key e Utilizando Lombok

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,26 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissoesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuladae;
+    // UMA MISSAO PARA VARIOS NINJAS. UM PARA VARIOS
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninja;
+}
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,33 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    int idade;
+    // @ManyToOne um ninja tem uma unica missão. Varios Para Um
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")// Foreing Key Chave estrangeira (Ou seja pegar uma coluna com a outra)
+    private MissoesModel missoes;
+
+
+}
